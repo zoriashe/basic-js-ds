120f1b524e3651a3dba35e931ee17747ff4237e9
zoriashe <zoriashee@gmail.com>
2023-04-15 18:52:48 +0600

    feat start binary tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -7,44 +7,150 @@ const { Node } = require('../extensions/list-tree.js');
 * using Node from extensions
 */
 class BinarySearchTree {
-
-  node = new Node()
+  constructor() {
+    this.root = null;
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.root.data
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    this.root = addWithin(this.root, data);
+
+    function addWithin(node, data) {
+      if (!node) {
+        return new Node(data);
+      }
+
+      if (node.data === data) {
+        return node;
+      }
+
+      if (data < node.data) {
+        node.left = addWithin(node.left, data);
+      } else {
+        node.right = addWithin(node.right, data);
+      }
+
+      return node;
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    return searchWithin(this.root, data);
+
+    function searchWithin(node, data) {
+      if (!node) {
+        return false;
+      }
+
+      if (node.data === data) {
+        return true;
+      }
+
+      return data < node.data ? 
+        searchWithin(node.left, data) : 
+        searchWithin(node.right, data);
+    }
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find( data ) {
+    return searchWithin(this.root, data);
+
+    function searchWithin(node, data) {
+      if (!node) {
+        return null;
+      }
+
+      if (node.data === data) {
+        return node.data;
+      }
+
+      return data < node.data ? 
+        searchWithin(node.left, data) : 
+        searchWithin(node.right, data);
+    }
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove( data ) {
+    this.root = removeNode(this.root, data);
+
+    function removeNode(node, data) {
+      if (!node) {
+        return null;
+      }
+
+      if (data < node.data) {
+        node.left = removeNode(node.left, data);
+        return node;
+      } else if (node.data < data) {
+        node.right = removeNode(node.right, data);
+        return node;
+      } else {
+
+        if (!node.left && !node.right) {
+          return null;
+        }
+
+        if (!node.left) {
+          node = node.right;
+          return node;
+        }
+
+        if (!node.right) {
+          node = node.left;
+          return node;
+        }
+
+        let minFromRight = node.right;
+        while (minFromRight.left) {
+          minFromRight = minFromRight.left;
+        }
+
+        node.data = minFromRight.data;
+
+        node.right = removeNode(node.right, minFromRight.data);
+
+        return node;
+      }
+    }
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.root) {
+      return;
+    }
+
+    let node = this.root;
+    while (node.left) {
+      node = node.left;
+    }
+
+    return node.data;
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.root) {
+      return;
+    }
+
+    let node = this.root;
+    while (node.right) {
+      node = node.right;
+    }
+
+    return node.data;
   }
 }
+
+const tree = new BinarySearchTree()
+
+tree.add(1)
+tree.add(4)
+tree.add(3)
+tree.add(5)
+tree.remove(4)
 
 module.exports = {
   BinarySearchTree
--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,8 +22,79 @@ const { ListNode } = require('../extensions/list-node.js');
  *   }
  * }
  */
+
+class LinkedList {
+  constructor() {
+    this.head = null
+    this.length = 0
+  }
+
+  add(value) {
+    if (this.length === 0) {
+      this.head = new ListNode(value);
+    } else {
+
+      let current = this.head;
+      while(current.next) {
+        current = current.next;
+      }
+
+      current.next = new ListNode(value);
+    }
+
+    this.length++;
+  }
+
+  rmAt(pos) {
+    if (pos < 0 || pos >= this.length) {
+      return;
+    }
+
+    let c = this.head
+
+    if (pos === 0) {
+      this.head = c.next
+    } else {
+      let pv = null
+      let i = 0
+
+      while (i < pos) {
+        pv = c
+        c = c.next
+        i++
+      }
+      pv.next = c.next
+    }
+    this.length--
+    return c.value
+  }
+
+  rm(el) {
+    while (this.indexOf(el) != -1) {
+      this.removeAt( this.indexOf(el) )
+    }
+  }
+
+  indexOf(el) {
+    let c = this.head
+    let i = 0
+
+    while (c) {
+      if (c.value === el) {
+        return i
+      }
+
+      c = c.next
+      i++
+    }
+  }
+}
+
 function removeKFromList(l, k) {
-  
+  let list = new LinkedList()
+  let {val, next} = l
+  list.add(l)
+  console.log(list)
   // remove line with error and write your code here
 }
 
